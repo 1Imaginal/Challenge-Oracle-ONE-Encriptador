a2e6474847d403da883366af08656d8b3afb6837
Juan Pablo <schism1@hotmail.com>
2023-06-23 20:27:59 -0600

    pequeñas mejoras en el layout e implementación de la encriptacion (Por arreglar)

--- encriptador.js
+++ encriptador.js
@@ -1,8 +1,23 @@
 function encriptar() {
     let texto = document.getElementById("input").value;
+    const key = new Map();
+    key.set('e', 'nter')
+    key.set('i', 'mes')
+    key.set('a', 'i')
+    key.set('o', 'ber')
+    key.set('u', 'fat')
+    num_caracteres = texto.length
+    texto_encriptado = texto;
+    let caracteres_agregados = 0;
+    for (let i = 0; i < num_caracteres; i++) {
+        if (key.has(texto[i])) {
+            caracteres_agregados += key.get(texto[i]).length;
+            texto_encriptado = texto_encriptado.slice(0,i + caracteres_agregados) + key.get(texto[i]) + texto.slice(i + caracteres_agregados);
+            console.log(texto_encriptado);
+        }
+    }
     let output_texto = document.getElementById("output")
-    texto = texto + "uwu"
-    output_texto.innerHTML = texto;
+    output_texto.innerHTML = texto_encriptado;
 }
 
 function desencriptar() {
