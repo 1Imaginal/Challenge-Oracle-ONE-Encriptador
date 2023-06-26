fe8da157abedb7fa20a7b9211f9f68483b99611a
Juan Pablo <schism1@hotmail.com>
2023-06-25 20:35:02 -0600

    Implementacion correcta de la funcion desencriptar y mejora a los botones

--- encriptador.js
+++ encriptador.js
@@ -1,22 +1,19 @@
+const key = new Map();
+key.set('e', 'nter');
+key.set('i', 'mes');
+key.set('a', 'i');
+key.set('o', 'ber');
+key.set('u', 'fat');
+
 function encriptar() {
     let texto = document.getElementById("input").value;
-    const key = new Map();
-    key.set('e', 'nter')
-    key.set('i', 'mes')
-    key.set('a', 'i')
-    key.set('o', 'ber')
-    key.set('u', 'fat')
-    num_caracteres = texto.length
+    num_caracteres = texto.length;
     texto_encriptado = texto;
     let caracteres_agregados = 0;
     for (let i = 0; i < num_caracteres; i++) {
         if (key.has(texto[i])) {
             texto_encriptado = texto_encriptado.slice(0,i + caracteres_agregados + 1) + key.get(texto[i]) + texto.slice(i + 1);
-            console.log(texto_encriptado.slice(0,i + caracteres_agregados + 1));
-            console.log(key.get(texto[i]))
-            console.log(texto.slice(i + caracteres_agregados + 1))
             caracteres_agregados += key.get(texto[i]).length;
-            console.log(caracteres_agregados)
         }
     }
     let output_texto = document.getElementById("output")
@@ -25,7 +22,15 @@ function encriptar() {
 
 function desencriptar() {
     let texto = document.getElementById("input").value;
+    texto_desencriptado = texto;
+    num_caracteres = texto.length;
+    let caracteres_agregados = 0;
+    for (let i = 0; i < num_caracteres; i++){
+        if (key.has(texto_desencriptado[i])) {
+            caracteres_agregados += key.get(texto_desencriptado[i]).length;
+            texto_desencriptado = texto_desencriptado.slice(0,i+1) + texto.slice(i + caracteres_agregados + 1);
+        }
+    }
     let output_texto = document.getElementById("output")
-    texto = texto - "uwu"
-    output_texto.innerHTML = texto;
+    output_texto.innerHTML = texto_desencriptado;
 }
